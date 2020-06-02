70b510e15cc4394492674dbd88be7011fcfbc0f9
francisco28 <fgallegosgo@gmail.com>
2020-06-02 12:21:37 -0600

    finishing the hook of useCoin

--- src/hooks/useCoin.js
+++ src/hooks/useCoin.js
@@ -1,4 +1,28 @@
 import React, { Fragment, useState } from 'react';
+import styled from '@emotion/styled';
+
+
+
+//style component
+const Label = styled.label`
+    font-family: 'Bebas Neue', cursive;
+    color: #FFF;
+    text-transform: uppercase;
+    font-weight: bold;
+    font-size: 2.4rem;
+    margin-top: 2rem;
+    display: block;
+`;
+
+const Selected = styled.select`
+    width: 100%;
+    display: block;
+    padding: 1rem;
+    -webkit-appearance: none;
+    border-radius: 10px;
+    border: none;
+    font-size: 1.2rem;
+`;
 
 const useCoin = (label, stateInitial, options) => {
 
@@ -8,8 +32,8 @@ const useCoin = (label, stateInitial, options) => {
 
     const Select = () => (
         <Fragment>
-            <label>{label}</label>
-            <select
+            <Label>{label}</Label>
+            <Selected
                 onChange={ e => updateState(e.target.value) }
                 value={state}
             >
@@ -17,7 +41,7 @@ const useCoin = (label, stateInitial, options) => {
                 { options.map(option => (
                     <option key={option.code} value={option.code}>{option.name}</option>
                 )) }
-            </select>
+            </Selected>
         </Fragment>
     );
 
